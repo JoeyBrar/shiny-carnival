0ce77e051f906b6f91b15824ff74db08c8fcf32e
rudradip ray <rudspam08@gmail.com>
2021-10-23 19:25:33 -0400

    define-function
    This function returns a set with information on words

--- DictionaryBot/define.py
+++ DictionaryBot/define.py
@@ -1,4 +1,10 @@
-import requests
-from bs4 import BeautifulSoup
+try:
+    from PyDictionary import PyDictionary
+except Exception as e:
+    print(f"{e}")
 
-URL = ''
+def define_word(word):
+    dictionary=PyDictionary()
+    full_definition = dictionary.meaning(word)
+    return full_definition
+print(define_word("dissonance"))
