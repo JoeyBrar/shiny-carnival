415f8579b077fc7de477df6688273bb858d25aec
rudradip ray <rudspam08@gmail.com>
2021-10-23 20:41:23 -0400

    final-define-update
    Returns strings with Discord Bot Response

--- DictionaryBot/define.py
+++ DictionaryBot/define.py
@@ -6,5 +6,10 @@ except Exception as e:
 def define_word(word):
     dictionary=PyDictionary()
     full_definition = dictionary.meaning(word)
-    return full_definition
-print(define_word("dissonance"))
+    #return full_definition
+    word_type = list(full_definition.keys())[0]
+    print(word_type)
+    lst_definitions = full_definition.get("Noun")
+    for i in lst_definitions:
+        print(i)
+    return "COMMAND DONE"
