2210f52baaa4fb5ac74d2b68cf2796d7fe4ede2b
JoeyBrar <51928278+JoeyBrar@users.noreply.github.com>
2021-11-21 23:16:42 -0500

    final function

--- DictionaryBot/main.py
+++ DictionaryBot/main.py
@@ -1,38 +1,42 @@
 try:
-    import requests
-    import os
-    import re
-    import discord
     from discord.ext import commands, tasks
-    from bs4 import BeautifulSoup
+    import define
+    import discord
 except Exception as e:
     print(f"{e}")
 
-
-client = commands.Bot(command_prefix = '.')
-['.help', 'type .help'] # For later
-
+client = commands.Bot(command_prefix = 'thicc ')
+x = ['thicc help', 'type thicc help']
 
 # Startup
 @client.event
 async def on_ready():
-    change_status.start()
+    switch.start()
     print('Bot is online.\n')
 
 @tasks.loop(seconds=10)
 async def switch():
-    for i in range(['.help', 'type .help']):
-        await client.status_change(activity=discord.Game(i))
+    for i in range(len(x)):
+        await client.change_presence(activity=discord.Game(i))
 
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send('pass in all required arguments pls')
 
+@client.command(help="- Don't know a word? Look it up here using the say command! Usage: thicc help (word)")
+async def say(ctx, *, question):
+    for index, i in enumerate(define.define_word(question)):
+        try:
+            await ctx.send((f"**Type #{index+1}** {i[0]} \n**Meaning(s) #{index+1}:** {' '.join(i)}"))
+        except:
+            print('check ur spelling')
 
-
-# Dictionary command:
-
+# ------------------------------------------------------------------------------------
+# Don't commit or push with actual token
+# DM me for token
+# Replace with 'test' after use
+client.run('test')
 
 
 # ------------------------------------------------------------------------------------
